7bfca08fddad33637e8f7d436e9203d428f19d89
Manash Roy <manashroy454@gmail.com>
2023-08-25 20:17:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="h-screen text-center text-3xl font-bold flex justify-center items-center underline">
+        React + Vite + TailwindCSS
+      </h1>
+    </>
+  )
+}
+
+export default App
